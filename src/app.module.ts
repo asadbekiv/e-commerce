394242738eb610051a6ee9b0bb7390493e19b9a4
Asadbek <ibodullaev.asadbek@mail.ru>
2025-03-12 14:31:16 +0500

    update: swagger docs,validation pipes add etc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,37 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './api/users/users.service';
-import { AuthService } from './api/auth/auth.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersModule } from './api/users/users.module';
 import { AuthModule } from './api/auth/auth.module';
 import { DatabaseModule } from './api/database/database.module';
 import { ProductModule } from './api/product/product.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './api/auth/auth.guard';
+import { RolesGuard } from './common/guards/roles.guard';
+import { LoggerMiddleware } from './middlewares/request-logger';
+import { CategoryService } from './api/category/category.service';
+import { CategoryModule } from './api/category/category.module';
+import { BasketController } from './api/basket/basket.controller';
+import { BasketModule } from './api/basket/basket.module';
+import { OrderService } from './api/order/order.service';
+import { OrderModule } from './api/order/order.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, DatabaseModule, ProductModule],
-  controllers: [],
-  providers: [UsersService, AuthService],
+  imports: [UsersModule, AuthModule, DatabaseModule, ProductModule, CategoryModule, BasketModule, OrderModule],
+  controllers: [BasketController],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+    CategoryService,
+    OrderService,
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as basicAuth from 'express-basic-auth';
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const docs_password = process.env.docs_password;
+
+  app.use(
+    '/api/docs*',
+    basicAuth({
+      challenge: true,
+      users: {
+        admin: docs_password,
+      },
+    }),
+  );
 
   await app.listen(process.env.PORT);
 }
